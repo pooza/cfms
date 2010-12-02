d7b3e90061f7919b5207474866ac95ad10b4d2aa
pooza <pooza@6bc21610-e001-0410-9273-fe8b54c00d2d>
2010-12-02 13:19:01 +0000

    git-svn-id: http://svn.b-shock.co.jp/commons/cfms2/trunk@4427 6bc21610-e001-0410-9273-fe8b54c00d2d

---
+++ webapp/modules/AdminProject/actions/DeleteImageAction.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * DeleteImageアクション
+ *
+ * @package jp.co.commons.cfms
+ * @subpackage AdminAccount
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ * @version $Id$
+ */
+class DeleteImageAction extends BSRecordAction {
+	public function execute () {
+		if ($file = $this->getRecord()->getImageFile($this->request['name'])) {
+			$file->delete();
+			$this->getRecord()->clearImageCache($this->request['name']);
+			$this->getRecord()->touch();
+		}
+
+		$url = $this->getModule()->getAction('Detail')->getURL();
+		$url->setParameter('pane', 'DetailForm');
+		return $url->redirect();
+	}
+
+	public function handleError () {
+		return $this->controller->getAction('not_found')->forward();
+	}
+}
+
+/* vim:set tabstop=4: */
