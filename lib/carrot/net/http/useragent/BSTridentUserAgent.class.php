d2d734cf932b2515cef5b7f6e0bbbc7464111814
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2012-01-11 19:01:22 +0900

    IE6のダウンロード時ファイル名対応

--- lib/carrot/net/http/useragent/BSTridentUserAgent.class.php
+++ lib/carrot/net/http/useragent/BSTridentUserAgent.class.php
@@ -46,12 +46,7 @@ class BSTridentUserAgent extends BSUserAgent {
 	 * @return string エンコード済みファイル名
 	 */
 	public function encodeFileName ($name) {
-		if (6 < $this->getVersion()) {
-			$name = BSURL::encode($name);
-		} else {
-			$name = BSString::convertEncoding($name, 'sjis-win');
-		}
-		return BSString::sanitize($name);
+		return BSURL::encode($name);
 	}
 
 	/**
